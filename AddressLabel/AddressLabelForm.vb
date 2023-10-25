35e9f1fbd4143bd1457c10fbd3303cb49d31966c
Zac <zac.minidude140@Gmail.com>
2023-10-25 14:00:26 -0600

    Updated Form Name

--- AddressLabel/AddressLabelForm.vb
+++ AddressLabel/AddressLabelForm.vb
@@ -3,7 +3,7 @@
 'Fall 2023
 'Address Label
 'https://github.com/Minidude140/AddressLabel.git
-Public Class AddressLabel
+Public Class AddressLabelForm
     'Custom Methods
     ''' <summary>
     ''' Clears any data entered in the text boxes
