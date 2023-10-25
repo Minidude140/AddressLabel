16a7bcdfc8bff6fe8327bb59ea1c09bb33e01101
Zac <zac.minidude140@Gmail.com>
2023-10-25 08:58:14 -0600

    Added Exit Button and SetDefaults() sub

--- AddressLabel/AddressLabel.vb
+++ AddressLabel/AddressLabel.vb
@@ -4,5 +4,23 @@
 'Address Label
 'https://github.com/Minidude140/AddressLabel.git
 Public Class AddressLabel
+    'Custom Subs
+    ''' <summary>
+    ''' Sets the Default State of Labels and Text Boxes
+    ''' </summary>
+    Sub SetDefaults()
+        DisplayLabel.Text = "Address Will Display Here"
+    End Sub
+    'Event Handlers
+    Private Sub AddressLabel_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SetDefaults()
+    End Sub
 
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        SetDefaults()
+    End Sub
 End Class
